5452da915d95c8d6a0b6cac0b6559e77ecf80e3b
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 21:29:26 +0530

    updated code

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,15 +5,16 @@ public class Fibonacci {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number of element in Fibonacci series: ");
         int count = in.nextInt();
-        int sum=0,n1=0,n2=1;
         System.out.println("Fibonacci Series: ");
-        for(int i=0;i<count;i++)
-        {
-            System.out.print(n1+" ");
-            sum = n1+n2;
+        fiboseries(count);
+    }
+    static void fiboseries(int n) {
+        int sum = 0, n1 = 0, n2 = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(n1 + " ");
+            sum = n1 + n2;
             n1 = n2;
             n2 = sum;
         }
-
     }
 }
