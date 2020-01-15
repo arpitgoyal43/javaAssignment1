89a5261e7129154c2942deb65b121528e8ee599d
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 21:57:54 +0530

    updated code

--- leapyear.java
+++ leapyear.java
@@ -1,21 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Leapyear
 {
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		System.out.println("Enter any Year: ");
-		Scanner in =  new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		int year = in.nextInt();
+		Leapyear leap = new Leapyear();
+		if(leap.isLeap(year))
+			System.out.println(year + " is a leap year");
+		else
+			System.out.println(year + " is not a leap year");
+	}
+
+	public boolean isLeap(int year){
 		if(year%4==0 || year%400==0)
 		{
 			
 			if(year%100!=0)
-				System.out.println(year + " is a leap year");
+				return true;
 			else
-				System.out.println(year + " is not a leap year");
+				return false;
 		}
 		else
-			System.out.println(year + " is not a leap year");	
+			return false;
 	}
 }		
