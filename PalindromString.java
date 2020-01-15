0bee5a0830108ab45c4ba099cb0b81f2269fe3a1
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 14:49:50 +0530

    check palindrom string  program

---
+++ PalindromString.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class PalindromString
+{
+	public static void main(String args[])
+	{
+		Scanner in =  new Scanner(System.in);
+		System.out.println("Enter any string: ");
+		String str = in.nextLine();
+		if(isPalindrom(str))
+			System.out.println(str + " is a palindrom string");
+		else
+			System.out.println(str + " is not a palindrom string");
+	
+	}
+
+	static boolean isPalindrom(String str)
+	{
+		int i=0,j=str.length()-1;		
+		while(i<j)
+		{
+			if(str.charAt(i) != str.charAt(j))
+				return false;
+			i++;
+			j--;
+		}
+		return true;	
+	}
+}
