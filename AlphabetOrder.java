356e8b2ad04d0d0a304bd547cd5914b314f70b89
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 16:02:27 +0530

    updated sort in alphabet order program1

--- AlphabetOrder.java
+++ AlphabetOrder.java
@@ -1,9 +1,13 @@
 import java.util.*;
 import java.io.*;
 
-class AlphabetOrder
-{
-	public static void main(String args[])
-	{
-		Scanner in =  new Scanner(System.in);
+class AlphabetOrder {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
 		System.out.println("Enter any string: ");
+		String str = in.nextLine();
+		char[] temp = str.toCharArray();
+		Arrays.sort(temp);
+		System.out.println(temp);
+	}
+}
