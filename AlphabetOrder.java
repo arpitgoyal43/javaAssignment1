97393b4fc14d52f0d104aebe72e02aec6dc76fa3
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 14:51:25 +0530

    sort in alphabet order program

---
+++ AlphabetOrder.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+class AlphabetOrder
+{
+	public static void main(String args[])
+	{
+		Scanner in =  new Scanner(System.in);
+		System.out.println("Enter any string: ");
