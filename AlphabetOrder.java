741a2c0282b9381e0ff1d8e3b34593d907778249
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 23:34:28 +0530

    updated code

--- AlphabetOrder.java
+++ AlphabetOrder.java
@@ -1,13 +1,36 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 class AlphabetOrder {
 	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
+		Scanner in1 = new Scanner(System.in);
+		System.out.println("Enter no. of strings: ");
+		int count = in1.nextInt();
+
 		System.out.println("Enter any string: ");
-		String str = in.nextLine();
-		char[] temp = str.toCharArray();
-		Arrays.sort(temp);
-		System.out.println(temp);
+		String[] str = new String[count];
+		Scanner in2 = new Scanner(System.in);
+		System.out.println("Enter the strings to be sorted in a sequence: ");
+		for (int i = 0; i <count; i++) {
+			str[i] = in2.nextLine();
+		}
+		String temp;
+		for(int i=0;i<count;i++)
+		{
+			for(int j=i+1;j<count;j++)
+			{
+				if(str[i].compareTo(str[j])>0)
+				{
+					temp = str[i];
+					str[i] = str[j];
+					str[j] = temp;
+				}
+			}
+		}
+		System.out.println("Sorted strings in alphabetical order: ");
+		for(int i=0;i<count;i++)
+		{
+			System.out.print(str[i]+ " ");
+		}
 	}
 }
