257129e49fcc94a21f65994dcf15045a3372b955
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 21:50:39 +0530

    updated code

--- FindDuplicates.java
+++ FindDuplicates.java
@@ -1,23 +1,24 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 class FindDuplicate
 {
-	public static void main(String args[])
-	{
-		Scanner in =  new Scanner(System.in);
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the String: ");
 		String str = in.nextLine();
-		char[] str_array = str.toCharArray();
-		int count=1;
+		FindDuplicate fd = new FindDuplicate();
 		System.out.println("Duplicate String: ");
+		fd.duplicatechar(str);
+	}
+
+	public void duplicatechar(String str){
+		char[] str_array = str.toCharArray();
 		for(int i=0;i<str.length();i++)
 		{
 			for(int j=i+1;j<str.length();j++)
 			{
 				if(str_array[i]==str_array[j])
 				{	System.out.print(str_array[j] + " ");
-					count++;
 					break;
 				}
 			}
