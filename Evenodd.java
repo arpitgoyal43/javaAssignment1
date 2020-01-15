60516ac720a5a521f7bdeefd428dde15bac229de
“Arpit <arpitgoyal437@gmail.com>
2020-01-15 12:32:17 +0530

    EvenOdd program

---
+++ Evenodd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Evenodd
+{
+	public static void main(String args[])
+	{
+		System.out.println("Enter any Number: ");
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		if(num%2==0)
+			System.out.println(num + " is an Even number.");
+		else
+			System.out.println(num + " is an Odd number.");
+	}
+}
