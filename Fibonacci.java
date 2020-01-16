6cdd1c7e4155c03783deb2fc7b78c4f6ea1db4a0
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 17:17:16 +0530

    updated code

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter number of element in Fibonacci series: ");
-        int count = in.nextInt();
+        int count = input.nextInt();
         System.out.println("Fibonacci Series: ");
-        fiboseries(count);
+        fibonacciSeries(count);
     }
-    static void fiboseries(int n) {
-        int sum = 0, n1 = 0, n2 = 1;
+
+    static void fibonacciSeries(int n) {
+        int sum, element1 = 0, element2 = 1;
         for (int i = 0; i < n; i++) {
-            System.out.print(n1 + " ");
-            sum = n1 + n2;
-            n1 = n2;
-            n2 = sum;
+            System.out.print(element1 + " ");
+            sum = element1 + element2;
+            element1 = element2;
+            element2 = sum;
         }
     }
 }
