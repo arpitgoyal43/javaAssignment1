e7b5423db66b47878cfd281804daa0c49b732070
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 17:25:05 +0530

    updated code

--- FindDuplicates.java
+++ FindDuplicates.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
-class FindDuplicate
-{
-	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the String: ");
-		String str = in.nextLine();
-		FindDuplicate fd = new FindDuplicate();
-		System.out.println("Duplicate String: ");
-		fd.duplicatechar(str);
-	}
+class FindDuplicate {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any word: ");
+        String word = input.nextLine();
+        FindDuplicate duplicate1 = new FindDuplicate();
+        System.out.println("Duplicate String: ");
+        duplicate1.duplicateCharacters(word);
+    }
 
-	public void duplicatechar(String str){
-		char[] str_array = str.toCharArray();
-		for(int i=0;i<str.length();i++)
-		{
-			for(int j=i+1;j<str.length();j++)
-			{
-				if(str_array[i]==str_array[j])
-				{	System.out.print(str_array[j] + " ");
-					break;
-				}
-			}
-		}
-	}
+    public void duplicateCharacters(String word) {
+        char[] word_array = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = i + 1; j < word.length(); j++) {
+                if (word_array[i] == word_array[j]) {
+                    System.out.print(word_array[j] + ", ");
+                    break;
+                }
+            }
+        }
+    }
 }	
