c0de885024b91e591a863ba985bea87dd068ea97
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 17:43:26 +0530

    updated code

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
-class SimpleInterest
-{
-	public static void main(String args[])
-	{
-		Scanner in =  new Scanner(System.in);
-		System.out.println("Enter the Principal: ");
-		double principle = in.nextDouble();
-		System.out.println("Enter the rate of sinterest: ");
-		double rate = in.nextDouble();
-		System.out.println("Enter the time: ");
-		double time = in.nextDouble();
-		double si = (principle * rate * time)/100;
-		System.out.println("Simple Interest: "+si);
-	}
+class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Principal: ");
+        double principle = input.nextDouble();
+        System.out.println("Enter the rate of interest: ");
+        float rate = input.nextFloat();
+        System.out.println("Enter the time: ");
+        int time = input.nextInt();
+        double simpleInterest;
+        simpleInterest = (principle * rate * time) / 100;
+        System.out.println("Simple Interest: " + simpleInterest);
+    }
 }
