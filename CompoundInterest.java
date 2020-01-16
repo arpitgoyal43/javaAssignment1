6b5f394d71ce23adae39bd83ac01cb9e0590e974
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 16:39:50 +0530

    updated code

--- CompoundInterest.java
+++ CompoundInterest.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
-class CompoundInterest
-{
-	public static void main(String args[])
-	{
-		Scanner in =  new Scanner(System.in);
-		System.out.println("Enter the Principal: ");
-		double principle = in.nextDouble();
-		System.out.println("Enter the rate of interest: ");
-		double rate = in.nextDouble();
-		System.out.println("Enter the time: ");
-		double time = in.nextDouble();
-		double ci = principle*(Math.pow((1+rate/100),time));
-		System.out.println("Compound Interest : " + ci);
-	}
-}
+class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the principal investment amount (the initial deposit or loan amount): ");
+        double principle = input.nextDouble();
+        System.out.println("Enter the annual interest rate (decimal): ");
+        float rate = input.nextFloat();
+        System.out.println("Enter the time: ");
+        int time = input.nextInt();
+        System.out.println("Enter the number of times that interest is compounded per unit time: ");
+        int n = input.nextInt();
 
+        //Formula: Amount = p(1+r/n)^nt
+        double amount = principle * (Math.pow((1 + rate / (100 * n)), (time * n)));
+        System.out.println("Compound Interest : " + amount);
+    }
+}
 
 
