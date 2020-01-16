89ec09206028deacb4f53f321e65b6b191cc9723
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 17:38:07 +0530

    updated code

--- ReverseString.java
+++ ReverseString.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
 
 public class ReverseString {
-    public static void main(String args[])
-    {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter any String: ");
-        String str = in.nextLine();
-        String[] words = str.split(" ");
-        String result = "";
-        for(int i=0;i<words.length;i++)
-        {
+        String forwardString = input.nextLine();
+        String[] words = forwardString.split(" ");
+        String reverseString = "";
+        for (int i = 0; i < words.length; i++) {
             String word = words[i];
             String reverseWord = "";
-            for(int j = word.length()-1;j>=0;j--)
-            {
+            for (int j = word.length() - 1; j >= 0; j--) {
                 reverseWord = reverseWord + word.charAt(j);
             }
-            result = result + reverseWord + " ";
+            reverseString = reverseString + reverseWord + " ";
         }
-        System.out.println("Reverse Words in String: "+ result);
+        System.out.println("Reverse Words in String: " + reverseString);
 
     }
 }
