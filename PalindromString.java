307093060bdbd9a573543cc9b492333c518b668d
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 13:00:39 +0530

    variables with valuable name and using reduced code

--- PalindromString.java
+++ PalindromString.java
@@ -4,25 +4,21 @@ class PalindromString
 {
 	public static void main(String args[])
 	{
-		Scanner in =  new Scanner(System.in);
-		System.out.println("Enter any string: ");
-		String str = in.nextLine();
-		if(isPalindrom(str))
-			System.out.println(str + " is a palindrom string");
+		Scanner input =  new Scanner(System.in);
+		System.out.println("Enter any word: ");
+		String word = input.nextLine();
+		if(isPalindrom(word))
+			System.out.println(word + " is a palindrom string");
 		else
-			System.out.println(str + " is not a palindrom string");
+			System.out.println(word + " is not a palindrom string");
 	
 	}
 
-	static boolean isPalindrom(String str)
+	static boolean isPalindrom(String word)
 	{
-		int i=0,j=str.length()-1;		
-		while(i<j)
-		{
-			if(str.charAt(i) != str.charAt(j))
+		for(int i=0, j=word.length()-1; i<j;i++,j--){
+			if(word.charAt(i) != word.charAt(j))
 				return false;
-			i++;
-			j--;
 		}
 		return true;	
 	}
