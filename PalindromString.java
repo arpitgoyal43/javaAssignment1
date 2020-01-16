083b8d092c8867612ec60caf716b6e306d8c443f
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 17:54:10 +0530

    updated code

--- PalindromString.java
+++ PalindromString.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
 
-class PalindromString
-{
-	public static void main(String args[])
-	{
-		Scanner input =  new Scanner(System.in);
-		System.out.println("Enter any word: ");
-		String word = input.nextLine();
-		if(isPalindrom(word))
-			System.out.println(word + " is a palindrom string");
-		else
-			System.out.println(word + " is not a palindrom string");
-	
-	}
+class PalindromString {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any word: ");
+        String word = input.nextLine();
+        if (isPalindrom(word))
+            System.out.println(word + " is a palindrom string");
+        else
+            System.out.println(word + " is not a palindrom string");
+    }
 
-	static boolean isPalindrom(String word)
-	{
-		for(int i=0, j=word.length()-1; i<j;i++,j--){
-			if(word.charAt(i) != word.charAt(j))
-				return false;
-		}
-		return true;	
-	}
+    static boolean isPalindrom(String word) {
+        for (int i = 0, j = word.length() - 1; i < j; i++, j--) {
+            if (word.charAt(i) != word.charAt(j))
+                return false;
+        }
+        return true;
+    }
 }
