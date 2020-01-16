5b35165671edd411431d8a029055c511a212749b
“Arpit <arpitgoyal437@gmail.com>
2020-01-16 17:03:16 +0530

    updated code

--- evenodd.java
+++ evenodd.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
-class Evenodd
-{
-	public static void main(String args[])
-	{
-		System.out.println("Enter any Number: ");
-		Scanner in = new Scanner(System.in);
-		int num = in.nextInt();
-		if(num%2==0)
-			System.out.println(num + " is an Even number.");
-		else
-			System.out.println(num + " is an Odd number.");
-	}
+class Evenodd {
+    public static void main(String[] args) {
+        System.out.println("Enter any Number: ");
+        Scanner input = new Scanner(System.in);
+        try {
+            int checknumber = input.nextInt();
+            if (checknumber % 2 == 0)
+                System.out.println(checknumber + " is an Even number");
+            else
+                System.out.println(checknumber + " is an Odd number");
+        } catch (Exception e) {
+            System.out.println("Please enter a valid number!");
+        }
+    }
 }
